9f0837d18ecd226c68e92f4ae4b03761b8ab5876
jayanacitra99 <jayana_putra_24rpl@student.smktelkom-mlg.sch.id>
2016-12-08 00:44:03 +0700

    Back One Step

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl605142332/wayangku/ProfileWayang.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl605142332/wayangku/ProfileWayang.java
@@ -1,13 +1,10 @@
 package id.sch.smktelkom_mlg.project.xirpl605142332.wayangku;
 
-import android.app.Activity;
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.RelativeLayout;
 
 /**
@@ -16,37 +13,18 @@ import android.widget.RelativeLayout;
 public class ProfileWayang extends Fragment {
 
     RelativeLayout view;
-    Activity context;
+
     public ProfileWayang() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        context = getActivity();
+
+        view = (RelativeLayout) inflater.inflate(R.layout.profile_wayang, container, false);
 
         getActivity().setTitle("Profile");
-        return inflater.inflate(R.layout.profile_wayang, container, false);
-    }
 
-    public void onStart() {
-        super.onStart();
-        Button bt = (Button) context.findViewById(R.id.bt);
-        bt.setOnClickListener(new OnClickListener() {
-            public void onClick(View view) {
-                Intent intent = new Intent(context, ProfilClass.class);
-                //add data to the Intent object
-                startActivity(intent);
-            }
-
-        });
-    }
-
-    private class OnClickListener implements View.OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            onStart();
-        }
+        return view;
     }
 }
