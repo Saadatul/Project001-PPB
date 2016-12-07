49e833fd9df2ffd0407bed180cd883ed8b4a10bd
Muhammad Gaizka Zaqhariou <muhammad_zaqhariou_24rpl@student.smktelkom-mlg.sch.id>
2016-12-07 18:17:23 +0700

    Cerita Wayang

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl605142332/wayangku/CeritaWayang.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl605142332/wayangku/CeritaWayang.java
@@ -1,11 +1,14 @@
 package id.sch.smktelkom_mlg.project.xirpl605142332.wayangku;
 
 import android.app.Fragment;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by -asus- on 11/17/2016.
@@ -13,6 +16,9 @@ import android.widget.RelativeLayout;
 public class CeritaWayang extends Fragment {
 
     RelativeLayout view;
+    Button B, M, P, G;
+    TextView hasil;
+    Resources resources;
 
     public CeritaWayang() {
     }
@@ -21,10 +27,56 @@ public class CeritaWayang extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        B = (Button) view.findViewById(R.id.buttonB);
+        M = (Button) view.findViewById(R.id.buttonM);
+        P = (Button) view.findViewById(R.id.buttonP);
+        G = (Button) view.findViewById(R.id.buttonG);
+        hasil = (TextView) view.findViewById(R.id.tvCerita);
+        final String[] MB = resources.getStringArray(R.array.Mahabarata);
+        final String[] BY = resources.getStringArray(R.array.Baratayudha);
+        final String[] PW = resources.getStringArray(R.array.Pandawa);
+
         view = (RelativeLayout) inflater.inflate(R.layout.cerita_wayang, container, false);
 
         getActivity().setTitle("Cerita Wayang");
 
+        B.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                doClick1();
+            }
+
+            private void doClick1() {
+                hasil.setText(BY.toString());
+            }
+        });
+        M.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                doClick2();
+                hasil = (TextView) view.findViewById(R.id.tvCerita);
+            }
+
+            private void doClick2() {
+                hasil.setText(MB.toString());
+            }
+        });
+        P.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                doClick3();
+                hasil = (TextView) view.findViewById(R.id.tvCerita);
+            }
+
+            private void doClick3() {
+                hasil.setText(PW.toString());
+            }
+        });
         return view;
     }
+
+
 }
